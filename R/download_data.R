7f7e73382ef6fc34d53599f28b8ee570fcaf557d
jeffpowell2 <jeffpowell2@gmail.com>
2022-06-02 09:09:22 +1000

    integrated covariates into main data and write to data folder

--- R/download_data.R
+++ R/download_data.R
@@ -1,8 +1,23 @@
-#downlaod the data from repo place it in data folder
+#download the data from repo place it in data folder
 
-if(!file.exists("data/data/decomp_with_covar.xlsx")) {
+if(!file.exists("data/global_termite_microbe_wd.xlsx")) {
   download.file("https://figshare.com/ndownloader/files/35496569",
-                "data/decomp_with_covar.xlsx")
+                "data/global_termite_microbe_wd.xlsx")
 }
 
-
+library(readxl)
+library(readr)
+library(dplyr)
+# library(tidyr)
+library(magrittr)
+read_excel("data/global_termite_microbe_wd.xlsx", sheet=1) %>% 
+  left_join(read_csv("data/covars_by_site.csv")) %>% 
+  rename(lat = latitude, 
+         long = longitude, 
+         trt = treatment, 
+         species = wood_used, 
+         termite_exposure = termite_discovery) %>% 
+  mutate(prec_m = prec/1000) %>% 
+  write_csv("data/decomp_with_covar.csv")
+  
+  
